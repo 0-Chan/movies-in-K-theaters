a1424fcfaa373366ca30b7a9034c85b14ed6f56c
0-Chan <kyc4421194@gmail.com>
2022-11-08 13:44:54 +0900

    Add simple svg icon to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,16 @@ export default function Header() {
       <header role="banner">
         <Link to="/">
           <svg>
-            <rect width="480" height="240" fill="#3d87fb" />
+            <polygon
+              points="10,25 90,25 80,120 20,120"
+              fill="#ff4122"
+            />
+            <path
+              d="M 15 25 Q 5 5 20 10 Q 25 0 30 10 Q 40 3 40 15 C 60 0 60 20 55 15 C 70 0 70 30 65 15 A 6 5 0 1 1 85 25"
+              fill="none"
+              stroke="#0a0a0a"
+              strokeWidth="6"
+            />
           </svg>
         </Link>
       </header>
