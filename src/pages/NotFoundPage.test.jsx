10fc31e2ab8d7a254164456e5399881208cf67ab
0-Chan <kyc4421194@gmail.com>
2022-11-08 13:45:36 +0900

    Add visible test to NotFoundPage

--- src/pages/NotFoundPage.test.jsx
+++ src/pages/NotFoundPage.test.jsx
@@ -9,7 +9,13 @@ describe('NotFoundPage', () => {
     expect(queryByText('404 Not Found')).not.toBeNull();
   });
 
-  it('img contains correct values', () => {
+  it('img is visible', () => {
+    const { getByRole } = render(<NotFoundPage />);
+
+    expect(getByRole('img')).toBeVisible();
+  });
+
+  it('img contains correct attribute values', () => {
     const { getByRole } = render(<NotFoundPage />);
 
     expect(getByRole('img')).toHaveAttribute('src', 'images/404.jpeg');
