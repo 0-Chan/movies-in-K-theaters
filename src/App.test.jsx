912cc1c946be796cf053c8494f56f3d6a253b438
0-Chan <kyc4421194@gmail.com>
2022-11-09 12:20:37 +0900

    Update App.test.jsx to cover /about page

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,16 +2,29 @@ import { MemoryRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
+
+import BOXOFFICE from '../fixtures/boxoffice';
+
+const key = 'boxOfficeResult.dailyBoxOfficeList';
+const keys = key.split('.');
+const DAILYBOXOFFICE = BOXOFFICE[keys[0]][keys[1]];
+
+jest.mock('react-redux');
 
 describe('App', () => {
   const dispatch = jest.fn();
 
   beforeEach(() => {
     dispatch.mockClear();
+
     useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      dailyBoxOffice: DAILYBOXOFFICE,
+    }));
   });
 
   function renderApp({ path }) {
@@ -23,10 +36,21 @@ describe('App', () => {
   }
 
   context('with path /', () => {
-    it('renders the home page', () => {
+    it('renders the / page', () => {
       const { container } = renderApp({ path: '/' });
 
-      expect(container).toHaveTextContent('헤더');
+      expect(dispatch).toBeCalled();
+
+      expect(container).toHaveTextContent('박스오피스');
+      expect(container).toHaveTextContent('미션임파서블');
+    });
+  });
+
+  context('with path /about', () => {
+    it('renders the /about page', () => {
+      const { container } = renderApp({ path: '/about' });
+
+      expect(container).toHaveTextContent('About');
     });
   });
 });
