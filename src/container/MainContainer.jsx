a766209ec966167cf69c136de5cdabba6f2b0f09
0-Chan <kyc4421194@gmail.com>
2022-11-13 23:59:02 +0900

    Add json to parse poster

--- src/container/MainContainer.jsx
+++ src/container/MainContainer.jsx
@@ -1,6 +1,10 @@
 import { useSelector } from 'react-redux';
 
+import { Link } from 'react-router-dom';
+
 import { get } from '../utils';
+
+import movies from '../data/moives';
 
 export default function MainContainer() {
   const dailyboxoffice = useSelector(get('dailyBoxOffice'));
@@ -11,6 +15,12 @@ export default function MainContainer() {
     );
   }
 
+  const hell = dailyboxoffice[0];
+  console.log('🚀 | dailyboxoffice', dailyboxoffice);
+  // console.log('🚀 | hell', hell && hell.movieNm);
+
+  // console.log(true && movies.filter((data) => data.title === hell.movieNm));
+
   return (
     <>
       <h2>
@@ -19,8 +29,25 @@ export default function MainContainer() {
       <ol>
         {dailyboxoffice.map((movie) => (
           <li key={movie.movieCd}>
+            <Link to={`/boxoffice/${movie.rank}`}>
+              <div>
+                { movies.filter((data) => data.title === movie.movieNm).map((user) => {
+                  return <p> {user.poster} </p>
+                }) }
+              </div>
+              {/* <img
+                src={
+                      `https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/
+                      86119/86119
+                      _320.jpg`
+                    }
+                alt="Movie postser"
+              /> */}
+            </Link>
             <p>
               제목:
+              {movie.movieCd}
+              <br />
               {movie.movieNm}
               개봉일:
               {movie.openDt}
