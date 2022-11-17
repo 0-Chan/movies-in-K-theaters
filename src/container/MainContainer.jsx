506bfb9a6c2c41c47a2dfb1a2b53298f9ad5af2b
0-Chan <kyc4421194@gmail.com>
2022-11-17 18:12:59 +0900

    Add MainContainer styles

--- src/container/MainContainer.jsx
+++ src/container/MainContainer.jsx
@@ -20,40 +20,50 @@ export default function MainContainer() {
   };
 
   return (
-    <>
-      <h2 className="text-3xl font-bold underline">
+    <div className="py-4 px-1 text-center bg-indigo-800 text-indigo-200">
+      <h1 className="text-5xl p-4 text-center font-bold bg-gradient-to-r from-purple-600 via-pink-600 to-blue-600 bg-clip-text text-transparent">
+        박스오피스 무비차트
+      </h1>
+      {/* <h2 className="text-2xl font-bold">
         박스오피스
-      </h2>
-      <ol className="grid grid-cols-3 lg:grid-cols-6 gap-4 lg:gap-8 relative -mx-10 -mb-5/16 pt-16 h-[670px] lg:h-auto">
+      </h2> */}
+      <ol className="grid grid-cols-2 lg:grid-cols-5 gap-2 lg:gap-4 relative">
         {dailyboxoffice.map((movie) => (
-          <li key={movie.movieCd}>
+          <li
+            key={movie.movieCd}
+            className="border-solid border-2 border-indigo-400"
+          >
             <Link to={`/boxoffice/${movie.rank}`}>
               { metadata.filter((datum) => datum.title === movie.movieNm).map((correspond) => (
-                <div key={correspond.poster}>
-                  <div className="relative p-2 md:p-4 bg-[#1D1E23] rounded">
-                    <img
-                      src={`https://img.cgv.co.kr/Movie/Thumbnail/Poster/0000${correspond.poster.slice(0, 2)}/${correspond.poster}/${correspond.poster}_320.jpg`}
-                      onError={handleError}
-                      alt="Movie postser"
-                    />
-                  </div>
-                  <div>
-                    {correspond.tags.map((tag) => <span key={tag}>{tag}</span>)}
-                  </div>
+                <div
+                  key={correspond.poster}
+                  className=""
+                >
+                  <img
+                    src={`https://img.cgv.co.kr/Movie/Thumbnail/Poster/0000${correspond.poster.slice(0, 2)}/${correspond.poster}/${correspond.poster}_320.jpg`}
+                    onError={handleError}
+                    alt="Movie postser"
+                    className="top-0 w-full h-full object-cover"
+                  />
+                  <p>
+                    {correspond.tags.map((tag) => <span className="bg-indigo-100 text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded" key={tag}>{tag}</span>)}
+                  </p>
                 </div>
               ))}
             </Link>
             <div>
-              제목 :
-              {movie.movieNm}
-              <br />
-              누적 관객수 :
-              { Number(movie.audiAcc) > 10000 ? Math.floor(Number(movie.audiAcc) / 10000) : 'Under 1' }
-              만
+              <h3 className="font-medium mb-1 text-white truncate">
+                {movie.movieNm}
+              </h3>
+              <p className="text-sm">
+                누적 관객 :
+                { Number(movie.audiAcc) > 10000 ? Math.floor(Number(movie.audiAcc) / 10000) : 'Under 1' }
+                만
+              </p>
             </div>
           </li>
         ))}
       </ol>
-    </>
+    </div>
   );
 }
