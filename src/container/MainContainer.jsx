0058dc39477d01c94928f9b52b4cc1c3f19c1d00
0-Chan <kyc4421194@gmail.com>
2022-12-22 21:57:11 +0900

    Add swiper to MainPage

--- src/container/MainContainer.jsx
+++ src/container/MainContainer.jsx
@@ -1,13 +1,20 @@
 import { useSelector } from 'react-redux';
-
 import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore,
+{
+  EffectCoverflow, Navigation, Autoplay,
+}
+  from 'swiper';
 
 import { get } from '../utils';
-
 import metadata from '../data/metadata';
+
+import 'swiper/swiper-bundle.min.css';
 
 export default function MainContainer() {
   const dailyboxoffice = useSelector(get('dailyBoxOffice'));
+  console.log('🚀 | dailyboxoffice', dailyboxoffice);
 
   if (!dailyboxoffice) {
     return (
@@ -17,58 +24,91 @@ export default function MainContainer() {
 
   const handleError = (e) => {
     e.target.src = '/images/onError-main.jpg';
+    e.target.onerror = null;
   };
 
+  SwiperCore.use([Autoplay]);
+
   return (
-    <div className="flex justify-center items-center flex-col h-screen">
-      <div className="max-w-screen-xl my-0 mx-auto w-full overflow-y-hidden">
-        <h1 className="text-6xl text-black mb-8 text-center font-bold">
-          박스오피스 무비챠-트
+    <div className="h-screen">
+      <div>
+        <h1 className="text-center font-semibold text-6xl text-gray-700">
+          TOP 박스오피스
         </h1>
       </div>
-      <div className="full-column max-w-full mx-auto py-1 md:py-3 px-1">
-        <div className="mb-12 md:mb-0" aria-roledescription="carousel">
-          <div className="relative">
-            <ul className="absolute flex mx-auto wh-full bg-smoky">
-              {dailyboxoffice.map((movie) => (
-                <li
-                  key={movie.movieCd}
-                  className="border-solid border-2 border-indigo-400"
-                >
-                  <Link to={`/boxoffice/${movie.rank}`}>
-                    {metadata.filter((datum) => datum.title === movie.movieNm).map((correspond) => (
-                      <div
-                        key={correspond.poster}
-                        className=""
-                      >
-                        <img
-                          src={`https://img.cgv.co.kr/Movie/Thumbnail/Poster/0000${correspond.poster.slice(0, 2)}/${correspond.poster}/${correspond.poster}_320.jpg`}
-                          onError={handleError}
-                          alt="Movie postser"
-                          className="top-0 w-full h-full object-cover"
-                        />
-                        <p>
-                          {correspond.tags.map((tag) => <span className="bg-indigo-100 text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded" key={tag}>{tag}</span>)}
-                        </p>
-                      </div>
-                    ))}
-                  </Link>
-                  <div>
-                    <h3 className="font-medium mb-1 text-white truncate">
-                      {movie.movieNm}
-                    </h3>
-                    <p className="text-sm">
-                      누적 관객 :
-                      { Number(movie.audiAcc) > 10000 ? Math.floor(Number(movie.audiAcc) / 10000) : 'Under 1' }
-                      만
-                    </p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
+
+      <Swiper
+        className="font-black"
+        spaceBetween={50}
+        slidesPerView="3"
+        effect="coverflow"
+        grabCursor
+        centeredSlides
+        navigation
+        modules={[EffectCoverflow, Navigation]}
+        pagination={{
+          clickable: true,
+          type: 'fraction',
+        }}
+        coverflowEffect={{
+          rotate: 45,
+          stretch: -100,
+          depth: 400,
+          modifier: 1,
+          slideShadows: false,
+          scale: 1,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: true,
+        }}
+      >
+        {dailyboxoffice.map((movie, index) => (
+          <SwiperSlide key={movie.movieCd}>
+            <div>
+              <div className="">
+                <img src={`https://top10.netflix.com/images/big_numbers/${index + 1}.png`} alt="number" width="64" height="64" className="object-cover self-start -mt-1 ml-7" />
+              </div>
+              <div className="">
+                <div>
+                  <h3 className="text-3xl text-center text-gray-900 truncate">
+                    {movie.movieNm}
+                  </h3>
+                  <p className="text-md text-gray-700 ml-3 mb-1">
+                    누적&nbsp;
+                    { Number(movie.audiAcc) > 10000 ? Math.floor(Number(movie.audiAcc) / 10000) : '1' }
+                    만
+                  </p>
+                </div>
+              </div>
+
+              <li key={movie.movieCd}>
+                <Link to={`/boxoffice/${movie.rank}`}>
+                  {metadata.filter((datum) => datum.title === movie.movieNm).map((correspond) => (
+                    <div key={correspond.poster} className="-mt-5">
+                      <img
+                        src={`https://img.cgv.co.kr/Movie/Thumbnail/Poster/0000${correspond.poster.slice(0, 2)}/${correspond.poster}/${correspond.poster}_320.jpg`}
+                        onError={handleError}
+                        alt="Movie postser"
+                        className="object-cover scale-95"
+                        width="512"
+                        height="256"
+                      />
+                      <p className="text-center -mt-2">
+                        {correspond.tags.map((tag) => <span className="bg-indigo-100 text-indigo-800 text-lg font-semibold mr-2 px-2.5 rounded" key={tag}>{tag}</span>)}
+                      </p>
+                    </div>
+                  ))}
+                </Link>
+              </li>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide> */}
     </div>
   );
 }
