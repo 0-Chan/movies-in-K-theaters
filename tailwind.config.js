957af59a721f5608ccc4d69aeaf9ddbef653168c
0-Chan <kyc4421194@gmail.com>
2022-12-22 10:01:56 +0900

    Fix lint warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ['./src/**/*.{html,js,jsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Roboto'],
+      body: ['"Open Sans"'],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
