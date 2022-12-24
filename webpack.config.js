9dfe325be42b254765f6f16942a9d9d182f84eb3
0-Chan <kyc4421194@gmail.com>
2022-12-24 12:55:17 +0900

    Add webpack build output directory

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   mode: 'production',
   entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, './build'),
+  },
   module: {
     rules: [
       {
