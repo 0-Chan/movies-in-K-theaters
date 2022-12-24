f6c695894b1d280ca6ce798e6bcf15be6908c121
0-Chan <kyc4421194@gmail.com>
2022-12-24 16:25:23 +0900

    Checking api Uncaught TypeError

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,10 @@ export default async function fetchDailyBoxOffice(targetDate) {
 
   const url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?'
   + `key=${API_KEY}&targetDt=${targetDate}`;
+  console.log('🚀 | url', url);
   const response = await fetch(url);
   const data = await response.json();
+  console.log('🚀 | data', data);
 
   return data.boxOfficeResult.dailyBoxOfficeList;
 }
